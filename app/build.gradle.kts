0d28075a983a4e4fa4e6e23cae7966183a5419fe
김영훈 <me@yeonghoon.kim>
2025-10-09 18:19:54 +0900

    chore: v1.18.0 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.reminder"
         minSdk = 26
         targetSdk = 34
-        versionCode = 19
-        versionName = "1.17.0"
+        versionCode = 20
+        versionName = "1.18.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
